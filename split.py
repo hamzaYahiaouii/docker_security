a4daba5c605465756f367ee78d2b995d2921324e
khireddine belkhiri <44124081+khireddine10@users.noreply.github.com>
2022-06-09 17:35:30 +0100

    Update split.py

--- split.py
+++ split.py
@@ -4,7 +4,7 @@ List = []
 with open('/tmp/log1', 'r') as file:
     data = file.read().split("*******",20)
     i = 1
-    while i < 8 :
+    while i < len(data) :
         List.append(data[i])
         print(List[i-1])
         i +=1
