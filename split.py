4ed82cac07fafaf5237901b711ac10bb9636eca0
khireddine belkhiri <44124081+khireddine10@users.noreply.github.com>
2022-06-10 02:45:04 +0100

    Update split.py

--- split.py
+++ split.py
@@ -1,10 +1,19 @@
+import os
 
 
-List = []
-with open('/tmp/log1', 'r') as file:
-    data = file.read().split("*******",20)
-    i = 1
-    while i < len(data) :
-        List.append(data[i])
-        print(List[i-1])
-        i +=1
+def read_log():
+    my_checks_db = dict()
+    for j in range(1, 8):
+        file = "/tmp/log{checkNum}".format(checkNum=j)
+        List = []
+        if os.path.exists(file):
+            with open(file, 'r') as file:
+                data = file.read().split("*******", 20)
+                i = 1
+                while i < len(data):
+                    List.append(data[i])
+                    i += 1
+            my_checks_db[j] = {}
+            for line in range(0, len(List)-1, 2):
+                my_checks_db[j][List[line]] = List[line+1]
+    return my_checks_db
